10c2c32969e853bcfa14d39d95a04831c53a486f
Daniel L. Alves <alves.lopes.dan@gmail.com>
2014-05-21 18:27:42 -0300

    Adding some unit tests

--- ALDBlurImageProcessorTests/ALDBlurImageProcessorTests.m
+++ ALDBlurImageProcessorTests/ALDBlurImageProcessorTests.m
@@ -11,17 +11,319 @@
 // ald
 #import "ALDBlurImageProcessor.h"
 
+#pragma mark - Globals
+
+static UIImage *testImage = nil;
+static UIImage *testRetinaImage = nil;
+
+static void *originalTestImagePixelData = NULL;
+static void *originalTestRetinaImagePixelData = NULL;
+
+#pragma mark - Interface
+
 @interface ALDBlurImageProcessorTests : XCTestCase
 @end
 
+#pragma mark - Implementation
+
 @implementation ALDBlurImageProcessorTests
+
++( void )setUp
+{
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+
+    CGContextRef testImageContext = CGBitmapContextCreate( NULL, 64, 64, 8, 0, colorSpace, ( CGBitmapInfo )kCGImageAlphaPremultipliedLast );
+    
+    CGFloat gradientColorComps[] = { 0.0f, 0.0f, 0.0f, 1.0f,
+                                     0.0f, 0.0f, 1.0f, 1.0f };
+    CGGradientRef gradient = CGGradientCreateWithColorComponents( colorSpace,
+                                                                  gradientColorComps,
+                                                                  NULL,
+                                                                  2 );
+    
+    CGColorSpaceRelease( colorSpace );
+    
+    CGContextDrawLinearGradient( testImageContext,
+                                 gradient,
+                                 CGPointMake( 32.0f, 0.0f ),
+                                 CGPointMake( 32.0f, 64.0f ),
+                                 0 );
+    
+    CGGradientRelease( gradient );
+    
+    CGImageRef testImageRef = CGBitmapContextCreateImage( testImageContext );
+    CGContextRelease( testImageContext );
+    
+    testImage = [UIImage imageWithCGImage: testImageRef scale: 1.0f orientation: UIImageOrientationUp];
+    testRetinaImage = [UIImage imageWithCGImage: testImageRef scale: 2.0f orientation: UIImageOrientationUp];
+    
+    CGImageRelease( testImageRef );
+    
+    originalTestImagePixelData = [self pixelDataFromImage: testImage pixelBufferSize: NULL];
+    originalTestRetinaImagePixelData = [self pixelDataFromImage: testRetinaImage pixelBufferSize: NULL];
+}
+
++( void )tearDown
+{
+    testImage = nil;
+    testRetinaImage = nil;
+    
+    if( originalTestImagePixelData )
+        free( originalTestImagePixelData );
+    
+    if( originalTestRetinaImagePixelData )
+        free( originalTestRetinaImagePixelData );
+}
 
 -( void )test_allows_empty_initialization
 {
+    XCTAssertNoThrow( [ALDBlurImageProcessor new] );
 }
 
--( void )test_allows_target_image_switching_after_initialization
+-( void )test_allows_target_image_switching_after_empty_initialization
 {
+    ALDBlurImageProcessor *blurProcessor = [ALDBlurImageProcessor new];
+    
+    XCTAssertNoThrow( blurProcessor.imageToProcess = testImage );
+}
+
+-( void )test_allows_target_image_switching_after_full_initialization
+{
+    ALDBlurImageProcessor *blurProcessor = [[ALDBlurImageProcessor alloc] initWithImage: testImage];
+    
+    XCTAssertNoThrow( blurProcessor.imageToProcess = testRetinaImage );
+}
+
+-( void )test_non_zero_even_radius_do_not_generate_errors_and_return_blurred_images
+{
+    ALDBlurImageProcessor *blurProcessor = [[ALDBlurImageProcessor alloc] initWithImage: testImage];
+
+    UIImage *blurredImage = [blurProcessor syncBlurWithRadius: 2 iterations: 1 errorCode: nil];
+    
+    XCTAssertNotEqual( blurProcessor.imageToProcess, blurredImage );
+    
+    int ret = [ALDBlurImageProcessorTests comparePixelDataFromImage: blurProcessor.imageToProcess
+                                               toPixelDataFromImage: blurredImage];
+    
+    XCTAssertNotEqual( ret, 0 );
+    
+    ALDBlurImageProcessor *retinaBlurProcessor = [[ALDBlurImageProcessor alloc] initWithImage: testRetinaImage];
+    
+    blurredImage = [retinaBlurProcessor syncBlurWithRadius: 2 iterations: 1 errorCode: nil];
+    
+    XCTAssertNotEqual( retinaBlurProcessor.imageToProcess, blurredImage );
+    
+    ret = [ALDBlurImageProcessorTests comparePixelDataFromImage: retinaBlurProcessor.imageToProcess
+                                           toPixelDataFromImage: blurredImage];
+    
+    XCTAssertNotEqual( ret, 0 );
+}
+
+-( void )test_non_zero_radius_non_zero_iterations_generates_new_blurred_image
+{
+    ALDBlurImageProcessor *blurProcessor = [[ALDBlurImageProcessor alloc] initWithImage: testRetinaImage];
+
+    UIImage *blurredImage = [blurProcessor syncBlurWithRadius: 1 iterations: 1 errorCode: nil];
+    
+    XCTAssertNotEqual( blurProcessor.imageToProcess, blurredImage );
+    
+    int ret = [ALDBlurImageProcessorTests comparePixelDataFromImage: blurProcessor.imageToProcess
+                                               toPixelDataFromImage: blurredImage];
+    
+    XCTAssertNotEqual( ret, 0 );
+    
+    blurredImage = [blurProcessor syncBlurWithRadius: 13 iterations: 5 errorCode: nil];
+    
+    XCTAssertNotEqual( blurProcessor.imageToProcess, blurredImage );
+    
+    ret = [ALDBlurImageProcessorTests comparePixelDataFromImage: blurProcessor.imageToProcess
+                                           toPixelDataFromImage: blurredImage];
+    
+    XCTAssertNotEqual( ret, 0 );
+}
+
+-( void )test_zero_radius_non_zero_iterations_returns_original_image
+{
+    ALDBlurImageProcessor *blurProcessor = [[ALDBlurImageProcessor alloc] initWithImage: testRetinaImage];
+    
+    UIImage *blurredImage = [blurProcessor syncBlurWithRadius: 0 iterations: 1 errorCode: nil];
+    
+    XCTAssertEqual( blurProcessor.imageToProcess, blurredImage );
+
+    int ret = [ALDBlurImageProcessorTests comparePixelDataFromImage: blurredImage
+                                                        toPixelData: originalTestRetinaImagePixelData];
+    
+    XCTAssertEqual( ret, 0 );
+    
+    blurredImage = [blurProcessor syncBlurWithRadius: 0 iterations: 3 errorCode: nil];
+    
+    XCTAssertEqual( blurProcessor.imageToProcess, blurredImage );
+
+    ret = [ALDBlurImageProcessorTests comparePixelDataFromImage: blurredImage
+                                                    toPixelData: originalTestRetinaImagePixelData];
+    
+    XCTAssertEqual( ret, 0 );
+}
+
+-( void )test_non_zero_radius_zero_iterations_returns_original_image
+{
+    ALDBlurImageProcessor *blurProcessor = [[ALDBlurImageProcessor alloc] initWithImage: testRetinaImage];
+    
+    UIImage *blurredImage = [blurProcessor syncBlurWithRadius: 1 iterations: 0 errorCode: nil];
+    
+    XCTAssertEqual( blurProcessor.imageToProcess, blurredImage );
+    
+    int ret = [ALDBlurImageProcessorTests comparePixelDataFromImage: blurredImage
+                                                        toPixelData: originalTestRetinaImagePixelData];
+    
+    XCTAssertEqual( ret, 0 );
+    
+    blurredImage = [blurProcessor syncBlurWithRadius: 7 iterations: 0 errorCode: nil];
+    
+    XCTAssertEqual( blurProcessor.imageToProcess, blurredImage );
+    
+    ret = [ALDBlurImageProcessorTests comparePixelDataFromImage: blurredImage
+                                                    toPixelData: originalTestRetinaImagePixelData];
+    
+    XCTAssertEqual( ret, 0 );
+}
+
+-( void )test_zero_radius_zero_iterations_returns_original_image
+{
+    ALDBlurImageProcessor *blurProcessor = [[ALDBlurImageProcessor alloc] initWithImage: testImage];
+    
+    UIImage *blurredImage = [blurProcessor syncBlurWithRadius: 0 iterations: 0 errorCode: nil];
+    
+    XCTAssertEqual( blurProcessor.imageToProcess, blurredImage );
+    
+    int ret = [ALDBlurImageProcessorTests comparePixelDataFromImage: blurredImage
+                                                        toPixelData: originalTestImagePixelData];
+    
+    XCTAssertEqual( ret, 0 );
+}
+
+-( void )test_throws_NSInvalidArgumentException_on_sync_blur_with_no_image_to_process
+{
+    ALDBlurImageProcessor *blurProcessor = [ALDBlurImageProcessor new];
+  
+    XCTAssertThrowsSpecificNamed( [blurProcessor syncBlurWithRadius: 9 iterations: 2 errorCode: nil], NSException, NSInvalidArgumentException );
+    
+    blurProcessor = [[ALDBlurImageProcessor alloc] initWithImage: testImage];
+    blurProcessor.imageToProcess = nil;
+    
+    XCTAssertThrowsSpecificNamed( [blurProcessor syncBlurWithRadius: 1 iterations: 1 errorCode: nil], NSException, NSInvalidArgumentException );
+}
+
+-( void )test_throws_NSInvalidArgumentException_on_async_blur_with_no_image_to_process
+{
+    ALDBlurImageProcessor *blurProcessor = [ALDBlurImageProcessor new];
+    
+    XCTAssertThrowsSpecificNamed( [blurProcessor asyncBlurWithRadius: 9 iterations: 2], NSException, NSInvalidArgumentException );
+    
+    blurProcessor = [[ALDBlurImageProcessor alloc] initWithImage: testImage];
+    blurProcessor.imageToProcess = nil;
+    
+    XCTAssertThrowsSpecificNamed( [blurProcessor asyncBlurWithRadius: 1 iterations: 1], NSException, NSInvalidArgumentException );
+    
+    blurProcessor = [ALDBlurImageProcessor new];
+    
+    XCTAssertThrowsSpecificNamed( [blurProcessor asyncBlurWithRadius: 9 iterations: 2 cancelingLastOperation: YES], NSException, NSInvalidArgumentException );
+    
+    blurProcessor = [[ALDBlurImageProcessor alloc] initWithImage: testImage];
+    blurProcessor.imageToProcess = nil;
+    
+    XCTAssertThrowsSpecificNamed( [blurProcessor asyncBlurWithRadius: 1 iterations: 1 cancelingLastOperation: YES], NSException, NSInvalidArgumentException );
+}
+
+#pragma mark - Helpers
+
++( int )comparePixelDataFromImage:( UIImage * )image1 toPixelDataFromImage:( UIImage * )image2
+{
+    size_t pixelBufferSize;
+    void *image1PixelData = [self pixelDataFromImage: image1 pixelBufferSize: &pixelBufferSize];
+    void *image2PixelData = [self pixelDataFromImage: image2 pixelBufferSize: nil];
+    
+    int ret = memcmp( image1PixelData, image2PixelData, pixelBufferSize );
+    
+    free( image1PixelData );
+    free( image2PixelData );
+    
+    return ret;
+}
+
++( int )comparePixelDataFromImage:( UIImage * )image toPixelData:( const void * )pixelData
+{
+    size_t pixelBufferSize;
+    void *imagePixelData = [self pixelDataFromImage: image pixelBufferSize: &pixelBufferSize];
+    
+    int ret = memcmp( imagePixelData, pixelData, pixelBufferSize );
+    
+    free( imagePixelData );
+    
+    return ret;
+}
+
++( void * )pixelDataFromImage:( UIImage * )image pixelBufferSize:( out size_t * )outPixelBufferSize
+{
+    CGImageRef imageRef = image.CGImage;
+    
+    size_t pixelBufferSize = CGImageGetBytesPerRow( imageRef ) * CGImageGetHeight( imageRef );
+    void *pixelBuffer = malloc( pixelBufferSize );
+
+    CFDataRef dataSource = CGDataProviderCopyData( CGImageGetDataProvider( imageRef ));
+    memcpy( pixelBuffer, CFDataGetBytePtr( dataSource ), pixelBufferSize );
+    CFRelease( dataSource );
+    
+    if( outPixelBufferSize )
+        *outPixelBufferSize = pixelBufferSize;
+    
+    return pixelBuffer;
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
